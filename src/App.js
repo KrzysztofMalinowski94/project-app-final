2b33ea7efee27f41fbc953135e3c0e8c67e5b5d2
Kris <krzysztof.malinowski94@gmail.com>
2022-08-17 11:22:08 +0200

    state implemented

--- src/App.js
+++ src/App.js
@@ -3,14 +3,41 @@ import "./App.css";
 
 export class App extends React.Component {
   
-	state ={ 
+	state ={
+    
+		//GLOBAL STATE
 		isLoading:true,
 		hasError: false,
 		errorMessage: "",
 		isInfoDisplayed: false,
 		infoMessage: "",
     
-    isUserLoggedIn:false,
+		//USER/AUTH STATE
+		isUserLoggedIn:false,
+		userDisplayName:"",
+		userEmail:"",
+		userAvatar: "",
+
+		//
+		notLoginRoute: "LOGIN", // 'NEW-ACCOUNT or FORGOT PASSWORD'
+
+		//LOGIN PAGE STATE
+		loginEmail:"",
+		loginPassword:"",
+
+
+		//CREATE ACCOUNT STATE
+		createAccountEmail:"",
+		createAccountPassword: "",
+		createAccountPasswordRepeat:"",
+
+		//RECOVER STATE
+		recoverPasswordEmail:"",
+
+		//COURSES STATE
+		courses: null,
+		searchPhrase:"",
+
 	};
 
 	render(){
