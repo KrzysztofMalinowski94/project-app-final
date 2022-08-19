fb8a0b8f231beecfded12a554848c51a1707d601
Kris <krzysztof.malinowski94@gmail.com>
2022-08-19 14:55:34 +0200

    error validation added to TextField

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -6,18 +6,30 @@ import classes from "./styles.module.css";
 export const TextField = (props) => {
 	const {
 		className,
+		errorMessage = "Error",
 		...otherProps
 	} = props;
 
 	return (
-		<input
-			className={`${classes.root}${className ? ` ${className}` : ""}`}
-			{...otherProps}
-		/>
+		<div className = {`${classes.root}${className ? ` ${classes.className}`: ""}`}>
+			<input
+				className={`${classes.input}${errorMessage ? ` ${classes.hasError}` : ""}`}
+				{...otherProps}
+			/>
+			{
+				errorMessage ?
+					<div
+						className={classes.errorMessage}>
+						{errorMessage}
+					</div> :
+					null
+			}
+		</div>
 	);
 };
 
 TextField.propTypes = {
+	errorMessage:PropTypes.string,
 	className: PropTypes.string
 };
 
